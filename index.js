5d08f4c95e6e1584d44d8162486abce7c669cf9d
rezzak ali <rezzak ali>
2023-05-01 19:21:40 +0530

    mongodb setup code push

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 // external import
 import colors from 'colors';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 
 // internal import
-
-// enable colors
-colors.enable();
+import connectDb from './config/db.js';
 
 // config dotenv
 dotenv.config();
 
+// enable colors
+colors.enable();
+
+// db connection
+connectDb();
+
 // port
 const PORT = process.env.PORT || 9000;
+// host
 const HOST_NAME = process.env.HOST_NAME || '127.0.0.1';
 
 // app object
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
 // route
 app.get('/', (req, res) => {
-  res.json('hello');
+  res.json('hello world');
 });
 
 // listening server
