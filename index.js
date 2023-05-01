d57639f7497162d7b0c7d6a0acff337092e6cae0
rezzak ali <rezzak ali>
2023-05-01 11:19:42 +0530

    server setup code push

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Hello');
+// external import
+import colors from 'colors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+// internal import
+
+// enable colors
+colors.enable();
+
+// config dotenv
+dotenv.config();
+
+// port
+const PORT = process.env.PORT || 9000;
+const HOST_NAME = process.env.HOST_NAME || '127.0.0.1';
+
+// app object
+const app = express();
+
+// route
+app.get('/', (req, res) => {
+  res.json('hello');
+});
+
+// listening server
+app.listen(PORT, HOST_NAME, () => {
+  console.log(
+    `Your server is running successfully at http://${HOST_NAME}:${PORT}`
+      .bgMagenta
+  );
+});
