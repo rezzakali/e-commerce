952c01f17972c9f8d673ebb82398534d3fdfb166
rezzak ali <rezzak ali>
2023-05-01 10:46:04 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+      <Button className="m-2">App Component</Button>
+    </div>
+  );
+}
+
+export default App;
